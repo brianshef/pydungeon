8f57750d26ad22023eca5b3989d6b36ecbe52080
Brian Shef <brianshef@outlook.com>
2025-07-30 13:58:04 -0500

    Initialize tcod window

--- main.py
+++ main.py
@@ -1,13 +1,36 @@
 #!/usr/bin/env python
 import tcod
 
+TITLE = "PyDungeon by Brian Shef"
+TILESHEET_FILE = "./assets/dejavu10x10_gs_tc.png"
 
-def main():
-    print("PyDungeon by Brian Shef")
+
+def main() -> None:
+    screen_width = 80
+    screen_height = 50
+
+    tileset = tcod.tileset.load_tilesheet(
+        TILESHEET_FILE, 32, 8, tcod.tileset.CHARMAP_TCOD
+    )
+
+    with tcod.context.new(
+        columns=screen_width,
+        rows=screen_height,
+        tileset=tileset,
+        title=TITLE,
+        vsync=True,
+    ) as context:
+        root_console = tcod.Console(screen_width, screen_height, order="F")
+        while True:
+            root_console.print(x=1, y=1, string="@")
+
+            context.present(root_console)
+
+            for event in tcod.event.wait():
+                if event.type == "QUIT":
+                    raise SystemExit()
+
 
 
 if __name__ == "__main__":
-    tcod.console_init_root(80, 50, "PyDungeon by Brian Shef", False)
     main()
-    tcod.console_flush()
-    tcod.console_wait_for_keypress(True)
