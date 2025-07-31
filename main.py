87ce5ff4bf0ce37d2a53fda50fc669cc8af52f39
Brian Shef <brianshef@outlook.com>
2025-07-31 09:31:19 -0500

    Refactor out engine from main

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import tcod
 
-from actions import EscapeAction, MovementAction
+from engine import Engine
 from entity import Entity
 from input_handlers import EventHandler
 
@@ -23,6 +23,8 @@ def main() -> None:
     npc = Entity(int(SCREEN_WIDTH / 2 - 5), int(SCREEN_HEIGHT / 2), "@", (255, 255, 0))
     entities = {npc, player}
 
+    engine = Engine(entities=entities, event_handler=event_handler, player=player)
+
     with tcod.context.new(
         columns=SCREEN_WIDTH,
         rows=SCREEN_HEIGHT,
@@ -32,25 +34,13 @@ def main() -> None:
     ) as context:
         root_console = tcod.console.Console(SCREEN_WIDTH, SCREEN_HEIGHT, order="F")
         while True:
-            root_console.print(x=player.x, y=player.y, text=player.char, fg=player.color)
+            engine.render(console=root_console, context=context)
 
-            context.present(root_console)
+            events = tcod.event.wait()
 
             root_console.clear()
 
-            for event in tcod.event.wait():
-
-                action = event_handler.dispatch(event)
-
-                if action is None:
-                    continue
-
-                if isinstance(action, MovementAction):                    
-                    player.move(dx=action.dx, dy=action.dy)
-
-                elif isinstance(action, EscapeAction):
-                    print("Escape action triggered, exiting...")
-                    raise SystemExit()
+            engine.handle_events(events)
 
 if __name__ == "__main__":
     main()
